2f12a56fdc55d89e7d7114c402d1c23d23c02768
Ricardo Signes <rjbs@cpan.org>
2012-07-03 18:51:17 -0400

    documentation

--- lib/Exception/Reporter.pm
+++ lib/Exception/Reporter.pm
@@ -3,7 +3,83 @@ use warnings;
 package Exception::Reporter;
 # ABSTRACT: a generic exception-reporting object
 
+=head1 SYNOPSIS
+
+First, you create a reporter.  Probably you stick it someplace globally
+accessible, like MyApp->reporter.
+
+  my $reporter = Exception::Reporter->new({
+    always_dump => { env => sub { \%ENV } },
+    sender      => [
+      Exception::Reporter::Sender::Email->new({
+        from => 'root',
+        to   => 'SysAdmins <sysadmins@example.com>',
+      }),
+    ],
+    summarizers => [
+      Exception::Reporter::Summarizer::Email->new,
+      Exception::Reporter::Summarizer::File->new,
+      Exception::Reporter::Summarizer::ExceptionClass->new,
+      Exception::Reporter::Summarizer::Fallback->new,
+    ],
+  });
+
+Later, some exception has been thrown!  Maybe it's an L<Exception::Class>-based
+exception, or a string, or a L<Throwable> object or who knows what.
+
+  try {
+    ...
+  } catch {
+    MyApp->reporter->report_exception(
+      [
+        [ exception => $_ ],
+        [ request   => $current_request ],
+        [ uploading => Exception::Reporter::Dumpable::File->new($filename) ],
+      ],
+    );
+  };
+
+The sysadmins will get a nice email report with all the dumped data, and
+reports will thread.  Awesome, right?
+
+=head OVERVIEW
+
+Exception::Reporter takes a bunch of input (the I<dumpables>) and tries to
+figure out how to summarize them and build them into a report to send to
+somebody.  Probably a human being.
+
+It does this with two kinds of plugins:  summarizers and senders.
+
+The summarizers' job is to convert each dumpable into a simple hashref
+describing it.  The senders' job is to take those hashrefs and send them to
+somebody who cares.
+
+=cut
+
 use Data::GUID guid_string => { -as => '_guid_string' };
+
+=method new
+
+  my $reporter = Exception::Reporter->new(\%arg);
+
+This returns a new reporter.  Valid arguments are:
+
+  summarizers - an arrayref of summarizer objects; required
+  senders     - an arrayref of sender objects; required
+  always_dump - a hashref of coderefs used to generate extra dumpables
+
+The C<always_dump> hashref bears a bit more explanation.  When
+C<L</report_exception>> is called, each entry in C<always_dump> will be
+evaluated and appended to the list of given dumpables.  This lets you make your
+reporter always include some more useful information.
+
+I<...but remember!>  The reporter is probably doing its job in a C<catch>
+block, which means that anything that might have been changed C<local>-ly in
+your C<try> block will I<not> be the same when evaluated as part of the
+C<always_dump> code.  This might not matter often, but keep it in mind when
+setting up your reporter.
+
+=cut
 
 sub new {
   my ($class, $arg) = @_;
@@ -34,7 +110,48 @@ sub new {
 }
 
 sub _summarizers { return @{ $_[0]->{summarizers} }; }
-sub _reporters   { return @{ $_[0]->{reporters} }; }
+sub _senders     { return @{ $_[0]->{senders} }; }
+
+=method report_exception
+
+  $reporter->report_exception(\@dumpables, $arg);
+
+This method makes the reporter do its job: summarize dumpables and send a
+report.
+
+Each entry in C<@dumpables> is expected to look like this:
+
+  [ $short_name, $value, \%arg ]
+
+The short name is used for a few things, including identifying the dumps inside
+the report produced.  It's okay to have duplicated short names.
+
+The value can, in theory, be I<anything>.  It can be C<undef>, any kind of
+object, or whatever you want to stick in a scalar.  It's possible that
+extremely exotic values could confuse the "fallback" summarizer of last resort,
+but for the most part, anything goes.
+
+The C<%arg> entry isn't used for anything by the core libraries that ship with
+Exception::Reporter, but you might want to use it for your own purposes.  Feel
+free.
+
+The reporter will try to summarize each dumpable by asking each summarizer, in
+order, whether it C<can_summarize> the dumpable.  If it can, it will be asked
+to C<summarize> the dumpable.  The summaries are collected into a structure
+that looks like this:
+
+  [
+    [ dumpable_short_name => \@summaries ],
+    ...
+  ]
+
+If a given dumpable can't be dumped by any summarizer, a not-very-useful
+placeholder is put in its place.
+
+The arrayref constructed is passed to the C<send_report> method of each sender,
+in turn.
+
+=cut
 
 sub report_exception {
   my ($self, $dumpables, $arg) = @_;
