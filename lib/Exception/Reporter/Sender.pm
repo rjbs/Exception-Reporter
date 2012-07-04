3d5549a04ead05fe31bdf12731338f60099927aa
Ricardo Signes <rjbs@cpan.org>
2012-07-03 21:47:54 -0400

    more docs

--- lib/Exception/Reporter/Sender.pm
+++ lib/Exception/Reporter/Sender.pm
@@ -1,5 +1,14 @@
 use strict;
 use warnings;
 package Exception::Reporter::Sender;
+# ABSTRACT: a thing that sends exception reports
+
+=head1 OVERVIEW
+
+This class exists almost entirely to allow C<isa>-checking.  It provides a
+C<new> method that returns a blessed, empty object.  Passing it any parameters
+will cause an exception to be thrown.
+
+=cut
 
 1;
--- lib/Exception/Reporter/Summarizer.pm
+++ lib/Exception/Reporter/Summarizer.pm
@@ -1,6 +1,20 @@
 use strict;
 use warnings;
 package Exception::Reporter::Summarizer;
+# ABSTRACT: a thing that summarizes dumpables for reporting
+
+use Carp ();
+
+=head1 OVERVIEW
+
+This class exists almost entirely to allow C<isa>-checking.  It provides a
+C<new> method that returns a blessed, empty object.  Passing it any parameters
+will cause an exception to be thrown.
+
+A C<sanitize_filename> method is also provided, which turns a vaguely
+filename-like string into a safer filename string.
+
+=cut
 
 sub new {
   my ($class, $arg) = @_;
